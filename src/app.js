9487ab5a67c57b262285377c91402c43d5a8b07c
Alejo <alejonahuelrott@gmail.com>
2024-10-08 23:29:03 -0300

    base api

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 import "dotenv/config.js";
 import express from "express";
 import morgan from "morgan";
+import authRoutes from "./routes/auth.routes.js";
+import tasksRoutes from "./routes/tasks.routes.js";
+import cors from "cors";
+import { FRONTEND_URL } from "./config.js";
 
-const app = express()
-app.use(morgan("dev"))
+const app = express();
 
-export default app
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/api", authRoutes);
+app.use("/api", tasksRoutes);
+
+export default app;
