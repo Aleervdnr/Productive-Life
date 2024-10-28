e90b495b4a4f959a5992db3cb48731ab55dcb079
Alejo <alejonahuelrott@gmail.com>
2024-10-28 10:05:15 -0300

    add message page in construction

---
+++ client/src/pages/GastosPage.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import mockupDesktop from "../assets/PLMockDesktop.png"
+import mockMobile from "../assets/PLMockMobile.png"
+import useWindowSize from "../hooks/useWindowSize";
+
+export default function GastosPage({ setActiveItem }) {
+  useEffect(() => {
+    setActiveItem("gastos");
+  }, []);
+
+  const { width } = useWindowSize();
+
+  return (
+    <div className="h-[calc(100vh-55px)] lg:h-screen w-full flex flex-col justify-center items-center font-medium px-5">
+      <img src={width > 475 ? mockupDesktop : mockMobile} alt="" className={`${width > 475 ? "w-[600px] " : "w-[250px]"} ${width > 1024 && "w-[800px]"}`} />
+      <p className="text-center">
+        ¡Estamos trabajando en esto! 🔧 Esta sección estará disponible pronto.
+        ¡Mantente atento a las novedades!
+      </p>
+    </div>
+  );
+}
