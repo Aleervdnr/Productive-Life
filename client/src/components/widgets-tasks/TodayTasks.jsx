158a9f105b7c2f336a76be007b9eb3daf7e9bf68
Alejo <alejonahuelrott@gmail.com>
2024-11-05 09:50:29 -0300

    sort tasks by hour

--- client/src/components/widgets-tasks/TodayTasks.jsx
+++ client/src/components/widgets-tasks/TodayTasks.jsx
@@ -33,16 +33,29 @@ export default function TodayTasks() {
     return messages[randomIndex];
   }
 
+  const filteredTasks = tasks
+    .filter((task) => task.taskDate == todayDate)
+    .sort((a, b) => {
+      // Convertir el tiempo de "HH:mm:ss" a un timestamp para ordenarlos
+      const timeA = new Date(`1970-01-01T${a.startTime}`).getTime();
+      const timeB = new Date(`1970-01-01T${b.startTime}`).getTime();
+      return timeA - timeB; // Orden ascendente
+    });
+
   return (
     <div className="max-lg:w-[100vw] max-lg:h-[calc(100dvh-85px)] px-5 lg:w-full lg:row-start-1 lg:row-end-3 lg:border-[2px] lg:border-dark-400 lg:rounded-lg lg:p-3 overflow-scroll">
       <span className="text-md font-semibold">Tareas del dia</span>
-      <div className={`${tasks.filter((task) => task.taskDate == todayDate).length ? "flex flex-col gap-2" : " w-full h-[calc(100%-32px)] grid place-content-center"}  mt-2`}>
-        {tasks.filter((task) => task.taskDate == todayDate).length ? (
-          tasks.map((task) =>
-            task.taskDate == todayDate ? (
-              <ItemTodayTask task={task} key={task._id} />
-            ) : null
-          )
+      <div
+        className={`${
+          tasks.filter((task) => task.taskDate == todayDate).length
+            ? "flex flex-col gap-2"
+            : " w-full h-[calc(100%-32px)] grid place-content-center"
+        }  mt-2`}
+      >
+        {filteredTasks.length ? (
+          filteredTasks.map((task) => (
+            <ItemTodayTask task={task} key={task._id} />
+          ))
         ) : (
           <p className="text-sm text-center">{getMotivationalMessage()}</p>
         )}
