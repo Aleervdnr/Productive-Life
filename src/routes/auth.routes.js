bdff8f7c06fc78babf78f9911920a9e09a687d6e
Alejo <alejonahuelrott@gmail.com>
2025-01-03 09:30:46 -0300

    resend email verification

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login, register, tourCompleted, verifyEmailToken, verifyToken } from "../controllers/auth.controllers.js";
+import { login, register, reSendEmailVerification, tourCompleted, verifyEmailToken, verifyToken } from "../controllers/auth.controllers.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
 
@@ -9,6 +9,7 @@ router.post("/register",validateSchema(registerSchema), register)
 router.post("/login",validateSchema(loginSchema), login)
 router.get("/verify-token", verifyToken)
 router.get("/verify-email-token", verifyEmailToken)
+router.post("/resend-email-verify", reSendEmailVerification)
 router.patch("/tour-completed", tourCompleted)
 
 export default router
